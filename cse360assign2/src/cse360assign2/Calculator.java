35639025671bf54f6ddedb254b474a59aca903c9
Neal M Lang <Neal M Lang@LAPTOP-12NB2T54.3521.dhcp.asu.edu>
2019-02-25 11:17:08 -0700

    Initial Push

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,44 @@
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+
+	}
+	
+	public int getTotal () {
+
+	}
+	
+	public void add (int value) {
+
+		
+
+	}
+	
+	public void subtract (int value) {
+
+		
+
+	}
+	
+	public void multiply (int value) {
+
+		
+
+	}
+	
+	public void divide (int value) {
+
+
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign2/src/cse360assign2/TestMain.java
@@ -0,0 +1,17 @@
+package cse360assign2;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		Calculator test = new Calculator();
+		test.add(4);
+		test.subtract(2);
+		test.multiply(-2);
+		test.divide(0);
+		
+		test.getTotal();
+		test.getHistory();
+
+	}
+
+}
